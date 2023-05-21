de1224e4ceb48d1161baf68bc31b9889273a4ece
altanemredemirci <altan_emre_1989@hotmail.com>
2023-05-21 15:48:57 +0300

    Loops || While

--- 08_ODEV_1/Program.cs
+++ 08_ODEV_1/Program.cs
@@ -11,14 +11,82 @@ namespace _08_ODEV_1
         static void Main(string[] args)
         {
             /*
-            *ÖDEV:
+            * ÖDEV:
              kullanıcıdan alınan cinsiyet bilgisine göre
              ==> ERKEK ise
                 yaşı 60 ve üstü ise maaşının 10 katı kadar ikramiye alaral emekli edilecek, yaş 60'ın altında ise çalıştığı gün sayısına göre eğer 6000 ve üstü ise maaşının 11 katı kadar ikramiye alarak emekli edilecek,    6000 altında ise emekli edilmeyecek bilgisi kullanıcıya gösterilecek
-                ==> KADIN ise
-                 yaşı 58 ve üstü ise maaşının 10 katı kadar ikramiye alarak emekli edilecek, yaş 58'ın altında ise çalıştığı gün sayısına göre eğer 5600 ve üstü ise maaşının 11 katı kadar ikramiye alarak emekli edilecek, 5600 altında ise emekli edilmeyecek bilgisi kullanıcıya gösterilecek
-                 ==> cinsiyet bilgisi switch-case ile sorgulanacak
+             ==> KADIN ise
+                yaşı 58 ve üstü ise maaşının 10 katı kadar ikramiye alarak emekli edilecek, yaş 58'ın altında ise çalıştığı gün sayısına göre eğer 4500 ve üstü ise maaşının 11 katı kadar ikramiye alarak emekli edilecek, 4500 altında ise emekli edilmeyecek bilgisi kullanıcıya gösterilecek
+             ==> cinsiyet bilgisi switch-case ile sorgulanacak
              */
+
+            Console.WriteLine("Cinsiyet(E/K):");
+            string cinsiyet = Console.ReadLine().ToUpper();
+
+            switch (cinsiyet)
+            {
+                case "E":
+                    Console.WriteLine("Yaşınız:");
+                    int yas = Convert.ToInt32(Console.ReadLine());
+                    if (yas >= 60)
+                    {
+                        Console.WriteLine("Maaşınız:");
+                        double maas = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("İKRAMİYE:"+(maas*10));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Prim Günü:?");
+                        int prim = Convert.ToInt32(Console.ReadLine());
+
+                        if (prim >= 6000)
+                        {
+                            Console.WriteLine("Maaşınız:");
+                            double maas = Convert.ToDouble(Console.ReadLine());
+
+                            Console.WriteLine("İKRAMİYE:" + (maas * 11));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Emeklilik Hayal!!");
+                        }
+                    }
+                    break;
+                case "K":
+                    Console.WriteLine("Yaşınız:");
+                    int yas2 = Convert.ToInt32(Console.ReadLine());
+                    if (yas2 >= 58)
+                    {
+                        Console.WriteLine("Maaşınız:");
+                        double maas = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("İKRAMİYE:" + (maas * 10));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Prim Günü:?");
+                        int prim = Convert.ToInt32(Console.ReadLine());
+
+                        if (prim >= 4500)
+                        {
+                            Console.WriteLine("Maaşınız:");
+                            double maas = Convert.ToDouble(Console.ReadLine());
+
+                            Console.WriteLine("İKRAMİYE:" + (maas * 11));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Emeklilik Hayal!!");
+                        }
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Hatalı Cinsiyet Bilgisi!!!.");
+                    break;
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ 09_Loops/Program.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Döngüler tekrar eden kod bloklarını tekrar tekrar yazmaktan bizi kurtarır. Belli bir tekrar(tur) sayısı belirlenir ve turun her adımında yapılacak işlem tanımlanır.
+
+            WHILE, FOR, DO WHILE, FOREACH
+             
+             */
+
+            //Ekrana 1 ile 10 arasındaki sayıları alt alta yazdırınız
+
+            //Console.WriteLine(1);
+            //Console.WriteLine(2);
+            //Console.WriteLine(3);
+            //Console.WriteLine(4);
+            //Console.WriteLine(5);
+            //Console.WriteLine(6);
+            //Console.WriteLine(7);
+            //Console.WriteLine(8);
+            //Console.WriteLine(9);
+            //Console.WriteLine(10);
+
+            #region WHILE
+
+            #region 1.Örnek
+            //int i = 1;
+
+            //while (i < 11)
+            //{                
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            #endregion
+
+            #region 2.Örnek
+
+            // 1 ile 100 arasındaki 3'ün katları olan sayıları ekrana yazdırınız.
+
+            //int s = 3;
+
+            //while (s < 100)
+            //{
+            //    Console.WriteLine(s);
+            //    s += 3; //s=s+3;
+            //}
+
+            //------------------------------------
+
+            //int s = 1;
+            //while (s<100)
+            //{
+            //    if (s % 3 == 0)
+            //    {
+            //        Console.WriteLine(s);
+            //    }
+
+            //    s++;
+            //}
+
+
+
+
+            #endregion
+
+            #region 3.Örnek
+
+            //1-100 arasındaki sayılardan 3'ün katları olanların toplamını ekrana yazdırınız.
+
+            //int s = 1; 
+            //int toplam = 0;
+            //while (s < 100)
+            //{
+            //    if (s % 3 == 0)
+            //    {
+            //        toplam = toplam + s;
+            //    }
+
+            //    s++;
+            //}
+            //Console.WriteLine("Toplam:"+toplam);
+            #endregion
+
+            #region 4.Örnek
+
+            // Kullanıcıya kaç adet ürün aldığını soralım. Girilen adet kadar kullanıcıya fiyat girmesini isteyelim ve toplam ödemesini ekrana yazdıralım.
+
+            //Console.WriteLine("Kaç adet ürün aldınız:");
+            //int adet = Convert.ToInt32(Console.ReadLine()); //5
+
+            //int s = 1;
+            //int toplam = 0;
+            //while (s <= adet)
+            //{
+            //    Console.WriteLine("Fiyat Gir");
+            //    int fiyat= Convert.ToInt32(Console.ReadLine());
+
+            //    toplam += fiyat;
+            //    s++;
+            //}
+            //Console.WriteLine("Toplam:" + toplam);
+
+
+
+            //Console.WriteLine("Kaç adet ürün aldınız:");
+            //int adet = Convert.ToInt32(Console.ReadLine()); //5
+
+            //double toplam = 0;
+            //while (0 < adet)
+            //{
+            //    Console.WriteLine("Fiyat Gir");
+            //    double fiyat = Convert.ToDouble(Console.ReadLine());
+
+            //    toplam += fiyat;
+            //    adet--;
+            //}
+            //Console.WriteLine("Toplam:" + toplam);
+
+
+            #endregion
+
+            #region 5.Örnek Kullanıcı Girişi
+
+            //BREAK: break komutu kendi içinde bulunduğu döngüyü kırar(bitirir).
+
+            // 3 giriş hakkı vererek kullanıcıadı ve şifre alıp giriş kontrolü yazınız
+
+            //string admin = "altanemre";
+            //string sifre = "1";
+            //int hak = 3;
+
+            //while (hak > 0)
+            //{
+            //    Console.WriteLine("Username:");
+            //    string username = Console.ReadLine();
+            //    Console.WriteLine("Password:");
+            //    string password = Console.ReadLine();
+
+            //    if (username == admin && sifre==password)
+            //    {
+            //        Console.WriteLine("Giriş Başarılı");
+            //        break; 
+            //    }
+            //    else if (hak == 1)
+            //    {
+            //        Console.WriteLine("Hakkınız Bitti");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Tekrar Deneyiniz");
+            //    }
+            //    hak--;
+            //    Console.WriteLine("Kalan Hakkınız:"+hak);
+            //}
+
+            /*
+            string dogruKullaniciAdi = "admin";            string dogruSifre = "123";            int girishakki = 3;            while (girishakki > 0)            {                Console.WriteLine("KullanıcıAdı:");                string KullaniciAdi = Console.ReadLine();                Console.WriteLine("Şifre:");                string sifre = Console.ReadLine();                if (KullaniciAdi == dogruKullaniciAdi && sifre == dogruSifre)
+                {                    Console.WriteLine("Giriş başarılı");                    break;
+                }                else
+                {                    Console.WriteLine("Giriş Başarısız! Kalan Giriş Hakkı: " + girishakki);                }                girishakki--;                Console.WriteLine("kalan hakknı:" + girishakki);                if (girishakki == 0)
+                {                    Console.WriteLine("Giriş hakkınız Bitti");                }
+
+            */
+
+
+
+
+            #endregion
+
+            #region 6.Örnek
+
+            //Continue: bulunduğu satırdan döngü koşuluna geri döner.
+
+            /*
+            //int i = 1;
+
+            //while (i<10)
+            //{
+            //    if (i == 7)
+            //    {
+            //        //i++;
+            //        continue;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+            */
+
+
+            // Kullanıcıdan 5 adet tek sayı alıp ekrana yazdırınız.
+
+            //int i = 1;
+            //while (i < 6) 
+            //{
+            //    Console.WriteLine("Tek sayı giriniz:");
+            //    int sayi = Convert.ToInt32(Console.ReadLine()); // 1,2
+
+            //    if (sayi % 2 == 1)
+            //    {
+            //        Console.WriteLine(sayi);
+            //    }
+            //    else
+            //    {
+            //        continue;
+            //    }
+            //    i++;
+            //}
+
+            #endregion
+
+            #region 7.Örnek
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Altan Emre HARİKA BİR ÖĞRETMENDİR. İNŞALLAH DAHA DA ZENGİN OLUR");
+            //    break;
+            //}
+
+
+            // Kullanıcı 0 değerini girene kadar girilen bütün sayıları toplayan döngü yapısını kodlayınız.
+            int toplam = 0;
+            while (true)
+            {
+                Console.WriteLine("bir sayı giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                if (sayi == 0)
+                {
+                    break;
+                }
+                toplam += sayi;
+            }
+
+            Console.WriteLine("Toplam:"+toplam);
+
+
+            #endregion
+
+            #endregion
+
+
+            //WHILE TRUE
+
+
+            Console.ReadLine();
+        }
+    }
+}
