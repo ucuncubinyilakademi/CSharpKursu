8d82d6f2e858148c9485ecfc3ec632d9b73621e8
altanemredemirci <altan_emre_1989@hotmail.com>
2023-06-03 15:58:00 +0300

    Exercise

--- 10_Arrays/Program.cs
+++ 10_Arrays/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -42,6 +43,114 @@ namespace _10_Arrays
             //    Console.WriteLine(isimler[i]);
             //}
 
+            #region ARRAY SINIFI ÖZELLİKLERİ VE METOTLARI
+
+            #region Clear()
+
+            // Verilen dizi içerisinde belirtilen index den başlayarak belirtilen uzunluk kadar data siler.
+            //string[] isimler = { "Altan", "Ceylan", "Uras", "Kıvanç", "Kerem" };
+
+            //for (int i = 0; i < isimler.Length; i++)
+            //{
+            //    Console.WriteLine(isimler[i]);
+            //}
+
+            //foreach(string aa in isimler)
+            //{
+            //    Console.WriteLine(aa);
+            //}
+
+
+
+            //Array.Clear(isimler,1,2);
+
+            //for (int i = 0; i < isimler.Length; i++)
+            //{
+            //    Console.WriteLine(isimler[i]);
+            //}
+
+
+
+            #endregion
+
+
+            #region Copy()
+            //string[] isimler = { "Altan", "Ceylan", "Uras", "Kıvanç", "Kerem" };
+
+            //string[] adlar = new string[10];
+            ////Array.Copy(isimler, adlar, 2);
+            //Array.Copy(isimler, 1, adlar, 4, 3);
+
+            ////foreach (string item in isimler)
+            ////{
+            ////    Console.WriteLine(item);
+            ////}
+
+            //foreach (string item in adlar)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+
+            #region IndexOf()
+
+            //IndexOf:Verilen dizideki istenilen elemanın ilk görüldüğü indexi
+
+            //string[] isimler = { "Altan", "Ceylan", "Uras", "Kıvanç", "Ceylan", "Kerem", "Ceylan" };
+
+            ////Console.WriteLine(Array.IndexOf(isimler,"Ceylan"));
+
+            ////int indx = Array.IndexOf(isimler, "Ceylan");
+            ////Console.WriteLine(indx);
+
+            //Console.WriteLine(Array.IndexOf(isimler, "Ceylan", 2));
+
+            #endregion
+
+            #region LastIndexOf()
+
+            //LastIndexOf:Verilen dizideki istenilen elemanın son görüldüğü indexi
+
+            //string[] isimler = { "Altan", "Ceylan", "Uras", "Kıvanç", "Ceylan", "Kerem","Ceylan" };
+
+            //Console.WriteLine(Array.LastIndexOf(isimler, "Ceylan"));
+
+            #endregion
+
+            #region ReSize()
+
+            //string[] isimler = { "Altan", "Ceylan", "Uras", "Kıvanç", "Kerem" };
+
+            //Array.Resize(ref isimler, 10);
+
+            //isimler[5] = "Duru";
+
+            //foreach (string item in isimler)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            #endregion
+
+            #region Sort() and Reverse()
+            //Sort: A->Z 0->9 sıralama yapar.
+            //Reverse: Tanımlı diziyi ters sıralar.(ters çevirir.)
+            //string[] isimler = { "Ceylan", "Uras", "Altan", "Kıvanç", "Kerem", "Işık", "Çağatay" };
+
+            //Array.Sort(isimler);
+            //Array.Reverse(isimler);
+            //foreach (string item in isimler)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            #endregion
+            #endregion
+
             #region 1.Örnek
 
             // 5 elemanlı bir diziye isim atayıp kullanıcıya gösteriniz
@@ -64,24 +173,49 @@ namespace _10_Arrays
             #region 2.Örnek
 
             // Aşağıdaki dizide tanımlı sayılardan çiftleri ayrı tekleri ayrı toplayarak sonucunu ekrana yazdırınız.
-            
-            int[] sayilar = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14 };
 
-            int cifttoplam = 0;
-            int tektoplam = 0;
+            //int[] sayilar = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14 };
+
+            //int cifttoplam = 0;
+            //int tektoplam = 0;
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    if (sayilar[i] % 2 == 0)
+            //    {
+            //        cifttoplam += sayilar[i];
+            //    }
+            //    else 
+            //    {
+            //        tektoplam += sayilar[i];
+            //    }
+            //}
+            //Console.WriteLine("TekToplam:"+tektoplam);
+            //Console.WriteLine("ÇiftToplam:"+cifttoplam);
+
+
+            #endregion
+
+            #region 3.Örnek
+            //bir dizinin eleman sayısı 10 ile 20 arsında olacak şekilde sistem tarafında otomatik atılmaktadır. Bu dizinin elemanlarını da sistem 0,1000 arasında otomatik atmaktadır.Otomatik oluşturulan bu dizinin eleman sayısını ve elemanlarını kullanıcıya gösteriniz
+
+            Random r = new Random();
+            int[] sayilar = new int[r.Next(10, 20)]; // 13
+
             for (int i = 0; i < sayilar.Length; i++)
             {
-                if (sayilar[i] % 2 == 0)
-                {
-                    cifttoplam += sayilar[i];
-                }
-                else 
-                {
-                    tektoplam += sayilar[i];
-                }
+                sayilar[i] = r.Next(0, 1000);
             }
-            Console.WriteLine("TekToplam:"+tektoplam);
-            Console.WriteLine("ÇiftToplam:"+cifttoplam);
+
+            foreach (int item in sayilar)
+            {
+                Console.WriteLine(item);
+            }
+
+
+           
+            
+
+
 
 
             #endregion
--- 10_Arrays_Odev_1/Program.cs
+++ 10_Arrays_Odev_1/Program.cs
@@ -21,8 +21,18 @@ namespace _10_Arrays_Odev_1
             *****
             ******
             *******
-           
+           */
 
+            //for (int i = 1; i <= 13; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            /*
             *******
             ******
             *****
@@ -31,6 +41,20 @@ namespace _10_Arrays_Odev_1
             **
             *
             
+            */
+            //for (int i = 13; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+
+
+            /*
 
 
                 *
@@ -42,15 +66,101 @@ namespace _10_Arrays_Odev_1
            
              */
 
+            //for (int i = 1; i <= 7; i++)
+            //{
+            //    for (int j = 1; j <= 7 - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //int x = 8;
+            //for (int i = 1; i <= x; i++)
+            //{
+            //    for (int j = 1; j <= (x - i); j++)
+            //        Console.Write(" ");
+
+            //    for (int t = 1; t < i * 2; t++)
+            //        Console.Write("*");
+            //    Console.WriteLine();
+            //}
+
+
             #endregion
 
             #region adınızın harflerini for döngüsü ile alt alta sıralayın 
 
+            //string ad = "AltanEmre"; // string aslında char'lar oluşan bir dizidir. string => char[]
+            //string[] isim = { "A", "l", "t","a","n" };
+
+            //for (int i = 0; i < ad.Length; i++)
+            //{
+            //    Console.WriteLine(ad[i]);
+            //}
+
             #endregion
 
-                    #region kullanıcıdan alınacak ürün sayısına göre ürün isimleri istenerek bir diziye atılacak ve sonrasında her bir ürün adı yazılarak karşısında fiyatı istendikten sonra bütün ürünlerin isimleri karşısında fiyatlarını listeleyiniz
+            #region kullanıcıdan alınacak ürün sayısına göre ürün isimleri istenerek bir diziye atılacak ve sonrasında her bir ürün adı yazılarak karşısında fiyatı istendikten sonra bütün ürünlerin isimleri karşısında fiyatlarını listeleyiniz
+            //Console.WriteLine("kaç adet ürün gireceksiniz");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
 
+            //string[] urun_adi = new string[sayi];
+            //int[] urun_fiyatı = new int[sayi];
+
+            //for (int i = 0; i < sayi; i++)
+            //{
+            //    Console.WriteLine((i + 1) + ". ürünün adının giriniz");
+            //    urun_adi[i] = Console.ReadLine();
+            //    Console.WriteLine((i + 1) + ". ürünün fiyatınnı giriniz");
+            //    urun_fiyatı[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //for (int i = 0; i < sayi; i++)
+            //{
+            //    Console.WriteLine(urun_adi[i] + " " + urun_fiyatı[i]);                
+            //}
+
+
+
+            //Console.Write("Kaç adet ürün gireceksiniz? ");
+            //int urunSayisi = int.Parse(Console.ReadLine());
+
+            //string[] urunIsimleri = new string[urunSayisi];
+            //decimal[] urunFiyatlari = new decimal[urunSayisi];
+
+            //for (int i = 0; i < urunSayisi; i++)
+            //{
+            //    Console.Write($"Ürün {i + 1} adı: ");
+            //    urunIsimleri[i] = Console.ReadLine();
+
+            //    Console.Write($"Ürün {i + 1} fiyatı: ");
+            //    urunFiyatlari[i] = decimal.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine($"Ürünlerin Listesi ve Fiyatları:");
+
+            //decimal toplamFiyat = 0;
+            //for (int i = 0; i < urunSayisi; i++)
+            //{
+            //    toplamFiyat += urunFiyatlari[i];
+            //}
+
+            //Console.WriteLine($"Toplam Fiyat: {toplamFiyat}₺");
             #endregion
+
+            #region Aşağıdaki dizideki tekrar eden dataların indexleri ekrana yazdırınız.
+
+            string[] isimler = { "Ali", "Veli", "Ali", "Ahmet", "Mehmet", "Ali", "Zehra", "Fatma", "Ayşe", "Ali" };
+            #endregion
+
+
+            Console.ReadKey();
+
         }
     }
 }
